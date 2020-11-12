94c6589ed7f2a00a9f3cdbf8360f30791963da96
Naelsum <david.bo@isep.fr>
2020-11-12 11:49:37 +0100

    my first commit

---
+++ blog/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+#Email form
+class EmailPostForms(forms.Form):
+  name = forms.CharField(max_length=25)
+  email = forms.EmailField()
+  to = forms.EmailField()
+  comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+  class Meta:
+    model = Comment
+    fields = ('name', 'email', 'body')
+
+# User form
+class SignUpForm(UserCreationForm):
+  first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+  last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+  email = forms.EmailField(max_length=254, help_text='Required Inform a valid email address')
+
+  class Meta:
+    model = User
+    fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
